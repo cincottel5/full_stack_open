0f378acf6955650f369edde3afad0d94511e35b9
Yelko Carvajal Mora <ycarvajalm@baccredomatic.com>
2024-04-25 14:58:32 -0600

    exercise 3.15 - 3.18

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -11,9 +11,10 @@ morgan.token('body', (request, response) => {
 })
 
 app.use(cors())
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.static("dist"))
+
 
 app.get('/', (request, response) => {
   response.send({message: 'server running', time: new Date()})
@@ -77,12 +78,40 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error)
+
+  if (error.name === 'CastError')
+    return response.status(400).send({error: 'malformatted id'})
+
+  next()
+}
+
+app.use(errorHandler)
 
 
 const port = process.env.PORT
